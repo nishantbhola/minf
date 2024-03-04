08e3f93ec1872c3c8b4fadcd6c87a7056331e69a
nishantbhola <99174347+nishantbhola@users.noreply.github.com>
2024-03-04 15:36:02 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,24 @@ import Footer from "./Footer";
 import MidSec from "./MidSec";
 import { Parallax } from "react-parallax";
 import { useState } from "react";
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 
 function Home() {
-
-
   const [open, setOpen] = useState(false);
 
   function isOpen() {
-    open ? setOpen(false) : setOpen(true)
+    open ? setOpen(false) : setOpen(true);
   }
 
-  
-
   return (
-    <div className="" >
-      <div className={`bg-neutral-900 md:hidden flex justify-center items-center h-[100vh] realative fixed z-50 ${open ? "w-[60%] transition-width duration-500" : "w-0 transition-width duration-500"} opacity-90 top-0 `}>
+    <div className="">
+      <div
+        className={`bg-neutral-900 md:hidden flex justify-center items-center h-[100vh] realative fixed z-50 ${
+          open
+            ? "w-[60%] transition-width duration-500"
+            : "w-0 transition-width duration-500"
+        } opacity-90 top-0 `}
+      >
         <div className="h-[100px] mobafter flex justify-center items-center w-[40px] bg-neutral-900 top-1/2 rounded-r-full  absolute right-[-39px]">
           <div class="ham">
             <input type="checkbox" onClick={isOpen} />
@@ -28,21 +31,29 @@ function Home() {
           </div>
 
           <svg xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
-            <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 56" id="menu">
+            <symbol
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 100 56"
+              id="menu"
+            >
               <path d="M48.33,45.6H18a14.17,14.17,0,0,1,0-28.34H78.86a17.37,17.37,0,0,1,0,34.74H42.33l-21-21.26L47.75,4" />
             </symbol>
           </svg>
         </div>
-            <div className={`${open? "grid" : "hidden"} text-white thermite text-center mt-[-70px] text-2xl gap-8`}>
-              <a href="/">EVENT</a>
-              <a href="/">RANKINGS</a>
-              <a href="/">NEWS</a>
-              <a href="/">CONNECT</a>
-              <a href="/">ABOUT</a>
-              <a href="/">CONTACT</a>
-            </div>
+        <div
+          className={`${
+            open ? "grid" : "hidden"
+          } text-white thermite text-center mt-[-70px] text-2xl gap-8`}
+        >
+          <a href="/">EVENT</a>
+          <a href="/">RANKINGS</a>
+          <a href="/">NEWS</a>
+          <a href="/">CONNECT</a>
+          <a href="/">ABOUT</a>
+          <a href="/">CONTACT</a>
+        </div>
       </div>
-      <Nav open={isOpen}/>
+      <Nav open={isOpen} />
       <div className={` mx-auto pb-5`}>
         <div className="thermite mx-auto flex justify-center">
           <span className="bg-[#EAEBED] px-6 py-4 md:px-12 md:py-6 text-4xl tracking-widest rounded-2xl md:text-6xl mb-[30px] mt-[-40px]">
@@ -62,12 +73,39 @@ function Home() {
             </span>
           </div>
         </Parallax>
+        <div className="text-4xl mt-8 md:text-5xl w-full flex justify-center items-center h-[80px] thermite">
+          WHAT'S TRENDING NOW
+        </div>
         <div className="container mx-auto px-5">
-          <MidSec />
+          <div>
+            <div className="flex mb-12 justify-between border-b-2 border-gray-600">
+              <p className="text-3xl text-gray-700 thermite pb-2">Featured</p>
+              <p className="text-gray-500 text-lg cursor-pointer thermite">
+                view all <KeyboardArrowRightRoundedIcon />
+              </p>
+            </div>
+            <MidSec />
+          </div>
+          <div>
+            <div className="flex mb-12 justify-between border-b-2 border-gray-600">
+              <p className="text-3xl text-gray-700 thermite pb-2">Events</p>
+              <p className="text-gray-500 text-lg cursor-pointer thermite">
+                view all <KeyboardArrowRightRoundedIcon />
+              </p>
+            </div>
+            <MidSec />
+          </div>
+          <div>
+            <div className="flex mb-12 justify-between border-b-2 border-gray-600">
+              <p className="text-3xl text-gray-700 thermite pb-2">Video</p>
+              <p className="text-gray-500 text-lg cursor-pointer thermite">
+                view all <KeyboardArrowRightRoundedIcon />
+              </p>
+            </div>
+            <MidSec />
+          </div>
         </div>
-        <div>
-          
-        </div>
+        <div></div>
       </div>
       <Footer />
     </div>
